389f580b1b483eca806dd584df1c27a1a157c34d
rshrest8 <rshrest8@msudenver.edu>
2020-05-20 16:57:57 -0600

    .gitignore working?

--- deliveryApp/deliveryAppApp/views.py
+++ deliveryApp/deliveryAppApp/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
   return redirect(vendor_home)
 
+@login_required(login_url='/vendor/sign-in')
 def vendor_home(request):
   return render(request, 'vendor/home.html')
