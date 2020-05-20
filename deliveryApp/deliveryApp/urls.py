4024d55a66573e9d2f0089f6d3d255a45c23e722
rshrest8 <rshrest8@msudenver.edu>
2020-05-20 16:01:26 -0600

    Simple login page added

--- deliveryApp/deliveryApp/urls.py
+++ deliveryApp/deliveryApp/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from deliveryAppApp import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('vendor/sign-in',
+          auth_views.LoginView.as_view(template_name='vendor/sign_in.html'),
+          name='vendor-sign-in'),
+    path('vendor/sign-out', auth_views.LogoutView.as_view(),
+         {'next_page': '/'},
+         name='vendor-sign-out'),
 ]
