0959d41aaf09b76b499922c215c7f184778d6e1a
rshrest8 <rshrest8@msudenver.edu>
2020-05-23 17:16:00 -0600

    Created vendor model and registered into admin panel

--- deliveryApp/deliveryAppApp/admin.py
+++ deliveryApp/deliveryAppApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from deliveryAppApp.models import Vendor
 
 # Register your models here.
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+  """Registers vendor model to admin panel"""
+
+  list_display = ('user', 'name', 'phone', 'address')
--- deliveryApp/deliveryAppApp/models.py
+++ deliveryApp/deliveryAppApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Vendor(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='vendor')
+  name = models.CharField(max_length=500)
+  phone = models.CharField(max_length=500)
+  address = models.CharField(max_length=500)
+  logo = models.ImageField(upload_to='vendor_logo/', blank=False)
+
+  def _str__(self):
+    return self.name
