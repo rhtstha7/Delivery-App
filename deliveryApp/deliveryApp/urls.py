973d371cc38e2fbf45993e3fd803da9b0bb2bbbe
rshrest8 <rshrest8@msudenver.edu>
2020-05-24 17:14:39 -0600

    Registration function added

--- deliveryApp/deliveryApp/urls.py
+++ deliveryApp/deliveryApp/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from deliveryAppApp import views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +32,4 @@ urlpatterns = [
     path('vendor/sign-up', views.vendor_sign_up,
          name='vendor-sign-up'),
     path('vendor/', views.vendor_home, name='vendor-home')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- deliveryApp/deliveryAppApp/forms.py
+++ deliveryApp/deliveryAppApp/forms.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from deliveryAppApp.models import Vendor
 
 class OwnerForm(forms.ModelForm):
+  email = forms.CharField(max_length=100, required=True)
+  password = forms.CharField(widget=forms.PasswordInput())
+
   class Meta:
     model = User
     fields = ("username", "password", "first_name", "last_name", "email")
