ff713001d070291055e5d8997d66e9dd2f83bf42
rshrest8 <rshrest8@msudenver.edu>
2020-06-01 07:19:10 -0600

    Added customer and driver models

--- deliveryAppApp/admin.py
+++ deliveryAppApp/admin.py
@@ -1,7 +1,7 @@
 """registers models to django admin site"""
 
 from django.contrib import admin
-from deliveryAppApp.models import Vendor
+from deliveryAppApp.models import Vendor, Customer, Driver
 
 
 # Register your models here.
@@ -10,3 +10,17 @@ class VendorAdmin(admin.ModelAdmin):
     """Registers vendor model to admin panel"""
 
     list_display = ('user', 'name', 'phone', 'address')
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    """Registers customer model to admin panel"""
+
+    list_display = ('user', 'avatar', 'phone', 'address')
+
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    """Registers driver model to admin panel"""
+
+    list_display = ('user', 'avatar', 'phone', 'address')
